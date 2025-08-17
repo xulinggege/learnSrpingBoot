d14abbe1ac928ffb2d8b24122e2680138d5d349d
xulingbo <407032002@qq.com>
2025-08-17 13:41:30 +0800

    AOP的核心概念及术语。 target。目标对象。需要被增强的类。 Aspect。切面。增强代码放置的类，就是切面类，使用@Aspect注解和@Component注解来修饰。 通知--advice。具体的实现方法。 	@Around环绕通知。在目标方法的前面和后面都可以增强， 	@Before: 目标方法之前执行 	@After：目标方法之后执行 	@AfterThrowing。目标方法出现了异常执行 	@AfterReturning。目标方法返回值执行 切点(Pointcut):切点就是要增强的哪些类和方法。使用execution来匹配和指定
    连接点：joinPoint。一个连接点总是代表一个方法的执行，可以获取到目标方法的信息，如获取方法的方法名称和参数。

--- c4_aop/src/main/java/com/xs/c4_aop/C4AopApplication.java
+++ c4_aop/src/main/java/com/xs/c4_aop/C4AopApplication.java
@@ -3,6 +3,7 @@ package com.xs.c4_aop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 启动类自动帮我门增加了@Configuration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
 public class C4AopApplication {
 
